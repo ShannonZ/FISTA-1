cb4929d9b8f0c9d01d6a0ac74ae67f58e929cfa7
Matthieu Vendeville <matthieu.vendeville@esat.kuleuven.be>
2018-02-23 14:05:59 +0100

    improved bit-exactness testing

--- matlab/test_mex_files.m
+++ matlab/test_mex_files.m
@@ -2,42 +2,51 @@
 
 %% Test X'*Y
 disp("Testing X'*Y")
-X=randn(64,200)';
-Y=randn(200,20000);
 
-tic
-XtY=mexCalcXtY(X,Y);
-t=toc;
-fprintf('mex-file time: %fs\n',t);
+tmex = 0;
+tmat = 0;
+for c = 1:10
+    X=randn(64,200)';
+    Y=randn(200,20000);
 
-tic
-XtY2=X'*Y;
-t=toc;
-fprintf('matlab-file time: %fs\n',t);
+    tic
+    XtY=mexCalcXtY(X,Y);
+    tmex = tmex + toc;
 
-diff = sum((XtY(:)-XtY2(:)).^2);
-if diff > 1.0e-10
-    warning('Error while testing Xt*Y');
+    tic
+    XtY2=X'*Y;
+    tmat=tmat+toc;
+
+    diff = sum((XtY(:)-XtY2(:)).^2);
+    if diff > 1.0e-10
+        warning('Error while testing Xt*Y');
+    end
 end
+fprintf('mex-file time: %fs\n',tmex);
+fprintf('matlab-file time: %fs\n\n',tmat);
+
 
 %% Test norm1
 disp("Testing norm1(X)")
-X=randn(64,200)';
+tmex = 0;
+tmat = 0;
+for c = 1:10
+    X=randn(64,200)';
+    tic
+    res=mexnorm1(X);
+    tmex = tmex + toc;
 
-tic
-res=mexnorm1(X);
-t=toc;
-fprintf('mex-file time: %fs\n',t);
-
-tic
-res2 = full(sum(abs(X(:))));
-t=toc;
-fprintf('matlab-file time: %fs\n',t);
-
-diff = abs(res2 - res);
-if diff > 1.0e-10
-    warning('Error while testing norm1(X)');
+    tic
+    res2 = full(sum(abs(X(:))));
+    tmat=tmat+toc;
+    
+    diff = abs(res2 - res);
+    if diff > 1.0e-10
+        warning('Error while testing norm1(X)');
+    end
 end
+fprintf('mex-file time: %fs\n',tmex);
+fprintf('matlab-file time: %fs\n\n',tmat);
 
 %% Test Gradiant
 disp("Testing Gradiant")
@@ -46,28 +55,33 @@ d      = 10; 	% data dimension
 N      = 20; 	% number of samples 
 k      = 30; 	% dictionary size 
 lambda = 0.01;
-Y      = normc(rand(d, N));
-D      = normc(rand(d, k));
 
-X=randn(k,N);
+tmex = 0;
+tmat = 0;
 
-DtD = D'*D;
-DtY = D'*Y;
+for c = 1:10
+    Y = normc(rand(d, N));
+    D = normc(rand(d, k));
+    X = randn(k,N);
 
-tic
-mexGradX=mexGradiant(X,DtD,DtY);
-t=toc;
-fprintf('matlab-file time: %fs\n',t);
+    DtD = D'*D;
+    DtY = D'*Y;
 
-tic
-GradX=grad(X,DtD,DtY);
-t=toc;
-fprintf('mex-file time: %fs\n',t);
+    tic
+    mexGradX=mexGradiant(X,DtD,DtY);
+    tmex = tmex + toc;;
 
-diff = sum((GradX(:)-mexGradX(:)).^2);
-if diff > 1.0e-10
-    warning('Error while testing Gradiant');
+    tic
+    GradX=grad(X,DtD,DtY);
+    tmat=tmat+toc;
+
+    diff = sum((GradX(:)-mexGradX(:)).^2);
+    if diff > 1.0e-10
+        warning('Error while testing Gradiant');
+    end
 end
+fprintf('mex-file time: %fs\n',tmex);
+fprintf('matlab-file time: %fs\n\n',tmat);
 
 %% Matlab functions
 %% to be moved in a separate file
