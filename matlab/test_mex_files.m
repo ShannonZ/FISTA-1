18edf3314580a145c4fd7116526bb97bf5d04a14
Matthieu Vendeville <matthieu.vendeville@esat.kuleuven.be>
2018-02-23 15:34:05 +0100

    Making sure needed dlls are next to mex files

--- matlab/test_mex_files.m
+++ matlab/test_mex_files.m
@@ -1,6 +1,11 @@
 %% Bit exactness testing against Matlab
 
-clear all
+clear all;
+
+% Making sure needed dlls are present next to mex files
+copyfile ../externals/blas_lapack_lib_win64/blas_win64_MT.dll .
+copyfile ../externals/blas_lapack_lib_win64/lapack_win64_MT.dll .
+
 addpath('../matlab_fista/')
 
 %% Test X'*Y
