731a969ba70a80e6c79ed105a9a0dc96929de19e
Matthieu Vendeville <matthieu.vendeville@esat.kuleuven.be>
2018-02-23 17:46:05 +0100

    Added tests for fista lasso function

--- matlab/build_mex_files.m
+++ matlab/build_mex_files.m
@@ -4,5 +4,6 @@ mex -g -I..\src -I../externals/armadillo -l..\solutions\x64\Debug\fista_lib.lib 
 mex -g -I..\src -I../externals/armadillo -l..\solutions\x64\Debug\fista_lib.lib -l../externals/blas_lapack_lib_win64/blas_win64_MT.lib  -l../externals/blas_lapack_lib_win64/lapack_win64_MT.lib mexnorm1.cpp
 mex -g -I..\src -I../externals/armadillo -l..\solutions\x64\Debug\fista_lib.lib -l../externals/blas_lapack_lib_win64/blas_win64_MT.lib  -l../externals/blas_lapack_lib_win64/lapack_win64_MT.lib mexGradiant.cpp
 mex -g -I..\src -I../externals/armadillo -l..\solutions\x64\Debug\fista_lib.lib -l../externals/blas_lapack_lib_win64/blas_win64_MT.lib  -l../externals/blas_lapack_lib_win64/lapack_win64_MT.lib mex_projl1.cpp
+mex -g -I..\src -I../externals/armadillo -l..\solutions\x64\Debug\fista_lib.lib -l../externals/blas_lapack_lib_win64/blas_win64_MT.lib  -l../externals/blas_lapack_lib_win64/lapack_win64_MT.lib mex_fista_lasso.cpp
 
 
